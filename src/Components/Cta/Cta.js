79180fdfd6db2aec4eb3b9e1b55633cd315ad2d4
QAZI ZOHAIB ALI <zebikazi33@gmail.com>
2023-08-31 01:51:21 +0500

    fix: responsiveness added on cta

--- src/Components/Cta/Cta.js
+++ src/Components/Cta/Cta.js
@@ -1,24 +1,33 @@
-import React from 'react'
-import bg from '../../Assets/Cta/cta-bg.png'
-import Button from '../Button/Button'
+import React from "react";
+import bg from "../../Assets/Cta/cta-bg.png";
+import Button from "../Button/Button";
+import "./cta.css";
 
 export default function Cta() {
   return (
-    <div className='flex justify-center'>
-    <div className='mx-auto'>
-      <img src={bg} className='relative lg:w-[1000px] filter brightness-[0.3]' alt="" />
-       <div className='absolute top-0 '> 
-       <div className='text-center text-white text-[50px]'>
-        Want to design for own ? <br /> calm, we can do it! 
-       </div>
-       <div>
-        ZALES
-       </div>
-       <div>
-        <Button variant="primary" title="Order Now!" otherclasses="py-2 italic" ></Button>
-       </div>
-       </div> 
+    <div className="flex justify-center">
+      <div className="mx-auto">
+        <img
+          src={bg}
+          className="relative min-h-[400px] max-h-[600px] object-cover lg:w-[1050px] filter brightness-[0.3]"
+          alt=""
+        />
+        <div className="relative -mt-[370px] md:-mt-[440px] lg:-mt-[540px]">
+          <div className="text-center cta-heading font-extrabold text-white text-[30px] md:text-[40px]  lg:text-[50px]">
+            Want to design for own ? <br className="hidden md:block" /> calm, we can do it!
+          </div>
+          <div className="text-[90px] text-center md:my-8 cta-bg-text font-extrabold text-white opacity-10">
+            ZALES
+          </div>
+          <div className="">
+            <Button
+              variant="primary"
+              title="Order Now!"
+              otherclasses="py-2 mx-auto md:mt-12 italic max-w-[140px]"
+            ></Button>
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
