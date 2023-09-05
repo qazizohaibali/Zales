5b05bcdc05bb4d59432708c0eb47cca7ce313d00
QAZI ZOHAIB ALI <zebikazi33@gmail.com>
2023-09-06 00:50:54 +0500

    fix: slider issues on WorksSection have been resolved

--- src/Components/OurWorks/OurWorks.js
+++ src/Components/OurWorks/OurWorks.js
@@ -14,18 +14,38 @@ import img3 from "../../Assets/works/img6.png";
 import img4 from "../../Assets/works/img4.png";
 import img5 from "../../Assets/works/img2.png";
 import img6 from "../../Assets/works/img3.png";
+import img7 from "../../Assets/works/img7.png";
+import img8 from "../../Assets/works/img8.png";
+import img9 from "../../Assets/works/img9.png";
 
 import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
 
 export default function OurWorks() {
   const work_images = [
+    { img: img4 },
     { img: img1 },
     { img: img2 },
     { img: img3 },
-    { img: img4 },
     { img: img5 },
+    { img: img9 },
     { img: img6 },
+    { img: img7 },
+    { img: img8 },
   ];
+
+  const swiper = {
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  };
 
   return (
     <div className=" ">
@@ -34,51 +54,40 @@ export default function OurWorks() {
           Zales combination of statements and simplistic style helps create a
           look that's as unique as you are
         </div>
-        <div className="ourwork-heading text-white text-[48px] md:text-[60px] lg:text-[80px] xl:text-[90px]">OUR WORKS</div>
+        <div className="ourwork-heading text-white text-[48px] md:text-[60px] lg:text-[80px] xl:text-[90px]">
+          OUR WORKS
+        </div>
       </div>
-      <Swiper
-        effect={"coverflow"}
-        grabCursor={true}
-        centeredSlides={true}
-        loop={true}
-        slidesPerView={"auto"}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 100,
-          modifier: 2.5,
-        }}
-        pagination={{ el: ".swiper-pagination", clickable: true }}
-        navigation={{
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-          clickable: true,
-        }}
-        modules={[EffectCoverflow, Pagination, Navigation]}
-        className="swiper_container"
-      >
-        {work_images.map(({ img }) => {
-          return (
-            <SwiperSlide className="flex justify-center items-center">
-              <img
-                src={img}
-                className="max-w-[300px] max-h-[400px]"
-                alt="slide_image"
-              />
-            </SwiperSlide>
-          );
-        })}
-
-        {/* <div className="slider-controler">
-        <div className="swiper-button-prev slider-arrow">
-          <ion-icon name="arrow-back-outline"></ion-icon>
-        </div>
-        <div className="swiper-button-next slider-arrow">
-          <ion-icon name="arrow-forward-outline"></ion-icon>
-        </div>
-        <div className="swiper-pagination"></div>
-      </div> */}
-      </Swiper>
+      <div className="relative owner_gallery_wrapper">
+        <Swiper
+          {...swiper}
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          loop={true}
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 0,
+            depth: 100,
+            modifier: 2.5,
+          }}
+          navigation={{
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+            clickable: true,
+          }}
+          modules={[EffectCoverflow, Pagination, Navigation]}
+          className="swiper_container"
+        >
+          {work_images.map(({ img }) => {
+            return (
+              <SwiperSlide className="flex justify-center items-center">
+                <img src={img} className="rounded-[10px]" alt="slide_image" />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
     </div>
   );
 }
